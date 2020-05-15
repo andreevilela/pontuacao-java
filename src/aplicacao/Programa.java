24d944375e53f5c172333a6dbc4a4966c2495b3d
Andre Vilela <andrevibegoogle@hotmail.com>
2020-05-15 20:00:21 -0300

    Leitura do Volume

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -11,9 +11,18 @@ public class Programa {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int qtdCompras;
+		double ticket;
+		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		System.out.println();
+		System.out.println("Quantas compras o cliente fez no último ano?");
+		qtdCompras = sc.nextInt();
+		System.out.println("Qual o ticket médio?");
+		ticket = sc.nextDouble();
 	}
 
 }
